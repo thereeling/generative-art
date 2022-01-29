ad8cf518a9d5da92ca459c6cfcece4517f66ed61
Nick Giuliani <nicholas.giuliani94@gmail.com>
2022-01-29 16:12:42 -0500

    initial implementation of canvas animation in react working, will incorporate mouse dragging art

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import Nav from './components/Nav';
 import CanvasOne from './pages/CanvasOne';
 
 function App() {
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.fillStyle = '#000000'
+    ctx.beginPath()
+    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
+    ctx.fill()
+  };
   return (
     <div className="App">
       <Router>
       <Nav />
       <Routes>
-        <Route exact path="/canvasone" element={<CanvasOne/>} />
+        <Route exact path="/canvasone" element={<CanvasOne draw={draw}/>} />
       </Routes>
       </Router>
     </div>
